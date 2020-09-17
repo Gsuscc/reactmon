57a022c7f63a9c6c88876a862de2ad1453c8b082
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-09-17 17:53:12 +0200

    Fixed pokekodex not detailing

--- src/components/Type.js
+++ src/components/Type.js
@@ -1,84 +1,73 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import "../index.css";
 
-export default class Type extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      typeLinks: [],
-      typeInfos: [],
-    };
-  }
+const Type = (props) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [typeLinks, setTypeLinks] = useState([]);
+  const [typeInfos, setTypeInfos] = useState([]);
 
-  getType() {
+  useEffect(() => {
     axios
       .get("https://pokeapi.co/api/v2/type")
       .then((response) => {
         response.data.results.forEach((type) => {
-          this.getTypeInfo(type.url);
+          axios.get(type.url).then((response) => {
+            let type = response.data;
+            setTypeInfos((typeInfos) => [...typeInfos, type]);
+            setIsLoading(false);
+          });
         });
         return response;
       })
       .then((response) => {
-        console.log("getting Links");
         const typeLinks = response.data.results;
-        this.setState({ typeLinks });
+        setTypeLinks(typeLinks);
       });
+  }, []);
+
+  // const getTypeInfo = (url) => {
+  //   axios.get(url).then((response) => {
+  //     let type = response.data;
+  //     setTypeInfos({ typeInfos: [...typeInfos, type] });
+  //     setIsLoading({ isLoading: false });
+  //   });
+  // };
+
+  if (isLoading) {
+    console.log(typeInfos);
+    console.log(typeLinks);
+    return <div className="Types">Loading...</div>;
   }
 
-  componentDidMount() {
-    this.getType();
-  }
+  console.log(typeInfos);
+  console.log(typeLinks);
 
-  //   getSpriteByName(name) {
-  //     for (let pokemon of this.state.typeInfos) {
-  //       console.log(pokemon);
-  //       if (pokemon.name === name) return pokemon.sprites.front_default;
-  //     }
-  //   }
-
-  getTypeInfo(url) {
-    axios.get(url).then((response) => {
-      let type = response.data;
-      this.setState({ typeInfos: [...this.state.typeInfos, type] });
-      this.setState({ isLoading: false });
-    });
-  }
-
-  render() {
-    const { isLoading, typeLinks } = this.state;
-
-    if (isLoading) {
-      return <div className="Types">Loading...</div>;
-    }
-
-    return (
-      <div className="container">
-        {console.log(this.state)}
-        {typeLinks.map((type) => (
-          <div className="Pokemons">
-            <p className="name">{type.name}</p>
-            {/* <div>
+  return (
+    <div className="container">
+      {typeLinks.map((type) => (
+        <div className="Pokemons">
+          <p className="name">{type.name}</p>
+          {/* <div>
               <img
                 className="pokeImg"
                 src={this.getSpriteByName(pokemon.name)}
                 alt="img"
               ></img>
             </div> */}
-            <Router>
-              <Link to={type.url}>
-                <button>Details</button>
-              </Link>
-            </Router>
-          </div>
-        ))}
-        <Link to={"/"}>
-          <button>Back</button>
-        </Link>
-      </div>
-    );
-  }
-}
+          <Router>
+            <Link to={type.url}>
+              <button>Details</button>
+            </Link>
+          </Router>
+        </div>
+      ))}
+      <Link to={"/"}>
+        <button>Back</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Type;

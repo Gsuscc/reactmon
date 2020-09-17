975d1705707c25a9dd7b3afb334533f1e7f2fb69
Gergely Molnar <molnar.gergo18@gmail.com>
2020-09-17 18:40:33 +0200

    add styled components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,31 @@ import Pokemons from "./components/Pokemons";
 import Type from "./components/Type";
 import DetailPage from "./components/DetailPage";
 import axios from "axios";
+import "./index.css";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-family: monospace;
+  font-size: 1.5rem;
+  border: solid black 1px;
+  border-radius: 21px;
+  text-align: center;
+  background-color: orange;
+  box-shadow: 21px;
+`;
+
+const Button = styled.button`
+  background-color: orangered;
+  text-decoration: none;
+  line-height: 30px;
+  border-radius: 10px;
+  margin: 5px 5px;
+  align-self: center;
+  &:hover {
+    background-color: green;
+    color: white;
+  }
+`;
 
 const App = (props) => {
   const [navbarLinks, setNavbarLinks] = useState([
@@ -39,15 +64,12 @@ const App = (props) => {
 
   const Home = (props) => {
     return (
-      <div>
+      <div className="navbar">
         {props.navbarLinks.map((link) => {
           return (
-            <li className="btn">
-              <Link to={link.link}>{link.buttonName}</Link>
-            </li>
-            // <button className="btn" onClick={this.displayPokemons}>
-            //   {link.buttonName}
-            // </button>
+            <Link to={link.link}>
+              <Button className="btn">{link.buttonName}</Button>
+            </Link>
           );
         })}
       </div>
@@ -56,6 +78,7 @@ const App = (props) => {
 
   let content = (
     <div className="App">
+      <H1>Pokemon Library</H1>
       <Router>
         <div>
           <Switch>
